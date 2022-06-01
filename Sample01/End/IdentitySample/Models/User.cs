c154526be7ac459c4cad36547551344da5d29f0b
buker2012 <buker2012@qq.com>
2022-06-01 12:39:49 +0800

    2022年6月1日

---
+++ Sample01/End/IdentitySample/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentitySample.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
