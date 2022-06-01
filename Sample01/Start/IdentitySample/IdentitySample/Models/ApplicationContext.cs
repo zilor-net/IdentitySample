2fa953be4b7260662e8da2c943c79ae4d0eafd7b
buker2012 <buker2012@qq.com>
2022-06-01 12:05:26 +0800

    2022年6月1日

---
+++ Sample01/Start/IdentitySample/IdentitySample/Models/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using IdentitySample.Models.Configuration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentitySample.Models
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        }
+
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
